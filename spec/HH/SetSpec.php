b0d6b524d45ce50736b92ccbfc22144190729b6a
Victor <victor@suumit.com>
2014-01-14 18:18:42 +0100

    Some more Set specs

--- spec/HH/SetSpec.php
+++ spec/HH/SetSpec.php
@@ -7,8 +7,33 @@ use Prophecy\Argument;
 
 class SetSpec extends ObjectBehavior
 {
-    function it_is_initializable()
-    {
+    function it_is_initializable() {
         $this->shouldHaveType('HH\Set');
+    }
+
+    function it_should_be_empty_on_initialization() {
+        $this->shouldHaveCount(0);
+    }
+
+    function it_should_allow_adding_values() {
+        $this->add(1);
+        $this->add(2);
+        $this->shouldHaveCount(2);
+        $this->contains(1)->shouldBe(true);
+        $this->contains(2)->shouldBe(true);
+    }
+
+    function it_should_not_contain_duplicate_values() {
+        $this->add('foo');
+        $this->add('foo');
+        $this->shouldHaveCount(1);
+        $this->contains('foo')->shouldBe(true);
+    }
+
+    function it_should_support_adding_an_array() {
+        $this->addAll(['foo', 'foo', 'bar']);
+        $this->shouldHaveCount(2);
+        $this->contains('foo')->shouldBe(true);
+        $this->contains('bar')->shouldBe(true);
     }
 }
